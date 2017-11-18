d71ca6b104ddcc66a83eb0d754e55b9b2cff3be9
Frank <akura11.tt@gmail.com>
2017-11-18 00:43:57 +0000

    Se llena la calificacion de los conjuntos con datos de buda

--- ckanext/mxopeness/plugin.py
+++ ckanext/mxopeness/plugin.py
@@ -2,8 +2,13 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
+def get_data_fusion_endpoint():
+    return "https://api.datos.gob.mx/v1/data-fusion"
+
+
 class MxopenessPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -11,3 +16,8 @@ class MxopenessPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'mxopeness')
+    
+    def get_helpers(self):
+        return {
+            'get_data_fusion_endpoint': get_data_fusion_endpoint
+        }
